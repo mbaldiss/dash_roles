54401b9025cfcdef4271f4ab1e29952ffe40aeef
Marco Baldissone <marcol2c4@gmail.com>
2022-05-15 18:39:06 -0300

    creamos una migracion y los campos para esa tabla

---
+++ database/migrations/2022_05_15_213348_create_blogs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            // despues de crear esta migracion, creamos los campos necesarios en la table
+            $table->string('titulo');
+            $table->text('contenido');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
